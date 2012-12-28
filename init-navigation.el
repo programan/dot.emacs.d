c482aec23c33758edbddc365815bdea218d29263
programan <github@programan.com>
2012-12-28 11:24:03 +0900

    emacs-navの幅を環境によって変えた

--- init-navigation.el
+++ init-navigation.el
@@ -1,4 +1,15 @@
 ;; emacs-nav
 (require 'nav)
-(setq nav-width 15) 
-;;(global-set-key "\C-x\C-d" 'nav-toggle)
+(cond
+ ((eq window-system 'w32)
+  (setq nav-width 20))
+ ((eq window-system 'ns)
+  (setq nav-width 15))
+ ((eq window-system 'mac)
+  (setq nav-width 15))
+ ((eq window-system 'x)
+  (setq nav-width 15)))
+
+(setq nav-split-window-direction 'vertical) ;; 分割したフレームを垂直に並べる
+;;(global-set-key "\C-x\C-d" 'nav-toggle)     ;; C-x C-d で nav をトグル 
+;;(global-set-key "\C-x\C-o" 'nav-toggle)
