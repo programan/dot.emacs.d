b0a576568db3ab094d470318ce95365963409ae3
programan <github@programan.com>
2012-12-28 15:32:59 +0900

    snippetの設定を変更

--- init-snippet.el
+++ init-snippet.el
@@ -2,10 +2,17 @@
 ;; snippet(部品)を内部に持っていてコード補完をしてくれる
 ;; Emacsのメニューを表示している場合項目が追加されている
 (require 'yasnippet) ;; not yasnippet-bundle
-(yas/initialize)
+
+(setq yas/snippet-dirs 
+      '("~/.emacs.d/site-lisp/yasnippet/snippets" "~/.emacs.d/site-lisp/yasnippet/extras/imported")
+      )
+
+;(yas/initialize)
 (yas/load-directory "~/.emacs.d/site-lisp/yasnippet/snippets")
 
 ;; rails-snippets
 ;; rails用のコード補完
 (yas/load-directory "~/.emacs.d/site-lisp/yasnippets-rails/rails-snippets")
+
+(yas-global-mode 1)
 
