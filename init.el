014df700e52793214231257caba70f1ae5307f1f
programan <github@programan.com>
2013-04-26 02:25:31 +0900

    バックアップとオートセーブファイルの設定を追加

--- init.el
+++ init.el
@@ -248,9 +248,15 @@
 (setq transient-mark-mode t)
 
 ;; *.~ とかのバックアップファイルを作らない
-(setq make-backup-files nil)
+;;(setq make-backup-files nil)
 ;; .#* とかのバックアップファイルを作らない
 ;(setq auto-save-default nil)
+
+;; バックアップとオートセーブファイルを~/.emacs.d/backups/へ集める
+(add-to-list 'backup-directory-alist
+	     (cons "." "~/.emacs.d/backups/"))
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
 
 
 ;; ファイルを訪問時のフックからvc-find-file-hookを削除
