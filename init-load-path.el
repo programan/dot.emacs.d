848b10b20da221f624ff65c997dfd2d78a19ed13
programan <github@programan.com>
2013-04-26 10:34:20 +0900

    rinariの読み込みを消去

--- init-load-path.el
+++ init-load-path.el
@@ -4,8 +4,6 @@
 ;;(add-to-list 'load-path (expand-file-name "~/elisp"))
 ;; Ruby
 (add-to-list 'load-path "~/.emacs.d/site-lisp/ruby/misc")
-;; Rinari
-;(add-to-list 'load-path "~/.emacs.d/site-lisp/rinari")
 ;; rhtml
 (add-to-list 'load-path "~/.emacs.d/site-lisp/rhtml")
 ;; yasnippet(コード補完)
