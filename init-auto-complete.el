e7947080aeaf295e80a5a8ca1747d8509aae5e46
programan <github@programan.com>
2013-04-26 13:24:10 +0900

    auto-completeにCakePHPの設定を追加

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -10,3 +10,14 @@
 (add-to-list 'ac-modes 'js2-mode)  ;; javascript用：js2-modeになったら、auto-completeをスタートさせる
 (add-to-list 'ac-modes 'jade-mode)  ;; jade用：jade-modeになったら、auto-completeをスタートさせる
 (add-to-list 'ac-modes 'coffee-mode)  ;; coffescript用：coffee-modeになったら、auto-completeをスタートさせる
+
+;; CakePHP
+(require 'ac-cake)
+(require 'ac-cake2)
+;; ac-cake用の関数定義
+(defun ac-cake-hook ()
+  (make-variable-buffer-local 'ac-sources)
+  (add-to-list 'ac-sources 'ac-source-cake)
+  (add-to-list 'ac-sources 'ac-source-cake2))
+;; CakePHPのauto-complete設定
+(add-hook 'php-mode-hook 'ac-cake-hook)
