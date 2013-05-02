28fd49da43c3025dc441957e42fa230567411a39
programan <github@programan.com>
2013-05-03 03:26:51 +0900

    Anythingの設定を変更、追加更新

--- init-anything.el
+++ init-anything.el
@@ -6,6 +6,29 @@
 ;; ctagsによる関数ジャンプ
 (global-set-key (kbd "M-.") 'anything-c-etags-select)
 
+;; 候補の最大表示数
+(setq anything-candidate-number-limit 100)
+
+;; root権限でアクションを実行する時のコマンド(su)
 (require 'anything-config nil t)
 (when (not (eq window-system 'w32))
   (setq anything-su-or-sudo "sudo"))
+
+;; describe-bindingsをAnythingに置き換える
+(when (require 'descbinds-anything nil t)
+  (descbinds-anything-install))
+
+;; Anythingのマッチング強化
+(require 'anything-match-plugin nil t)
+
+;; Lispシンボルの補完候補の再検索時間
+(when (require 'anything-complete nil t)
+  (anything-lisp-complete-symbol-set-timer 150))
+
+;; 補完インタフェースの拡張
+(require 'anything-show-completion nil t)
+
+;; auto-installのAnythingインタフェース
+(when (require 'auto-install nil t)
+  (require 'anything-auto-install nil t))
+
