6009d31f423c4449cd2b078ead0a84e2ea3b7168
programan <github@programan.com>
2013-05-03 19:51:25 +0900

    yasnippetの初期化部分を修正

--- init-snippet.el
+++ init-snippet.el
@@ -1,17 +1,17 @@
 ;; yasnippet
 ;; snippet(部品)を内部に持っていてコード補完をしてくれる
 ;; Emacsのメニューを表示している場合項目が追加されている
-(require 'yasnippet) ;; not yasnippet-bundle
-(yas-global-mode 1)
-;(yas-reload-all)
+(when (require 'yasnippet nil t) ;; not yasnippet-bundle
+  (yas-global-mode 1)
+  ;; (yas-reload-all)
 
-(setq yas/snippet-dirs 
-      '("~/.emacs.d/site-lisp/yasnippet/snippets" "~/.emacs.d/site-lisp/yasnippet/extras/imported")
-      )
+  (setq yas/snippet-dirs 
+	'("~/.emacs.d/site-lisp/yasnippet/snippets" "~/.emacs.d/site-lisp/yasnippet/extras/imported")
+	)
 
-;(yas/initialize)
-(yas/load-directory "~/.emacs.d/site-lisp/yasnippet/snippets")
+  ;; (yas/initialize)
+  (yas/load-directory "~/.emacs.d/site-lisp/yasnippet/snippets")
 
-;; rails-snippets
-;; rails用のコード補完
-(yas/load-directory "~/.emacs.d/site-lisp/yasnippets-rails/rails-snippets")
+  ;; rails-snippets
+  ;; rails用のコード補完
+  (yas/load-directory "~/.emacs.d/site-lisp/yasnippets-rails/rails-snippets"))
