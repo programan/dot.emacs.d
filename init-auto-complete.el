e9906df102be5ccb37b10de5f454bd90741cd2db
programan <github@programan.com>
2013-05-30 12:00:35 +0900

    lua-mode時にauto-completeを有効化

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -4,8 +4,9 @@
   (ac-config-default)
   ;; (require 'ac-python)               ;; python用の補完
   ;; (add-to-list 'ac-modes 'python-2-mode)
-  (add-to-list 'ac-modes 'php-mode)  ;; PHP用：php-modeになったら、auto-completeをスタートさせる
-  (add-to-list 'ac-modes 'ruby-mode)  ;; Ruby用：ruby-modeになったら、auto-completeをスタートさせる
-  (add-to-list 'ac-modes 'js2-mode)  ;; javascript用：js2-modeになったら、auto-completeをスタートさせる
-  (add-to-list 'ac-modes 'jade-mode)  ;; jade用：jade-modeになったら、auto-completeをスタートさせる
-  (add-to-list 'ac-modes 'coffee-mode))  ;; coffescript用：coffee-modeになったら、auto-completeをスタートさせる
+  (add-to-list 'ac-modes 'php-mode)	;; PHP用：php-modeになったら、auto-completeをスタートさせる
+  (add-to-list 'ac-modes 'ruby-mode)	;; Ruby用：ruby-modeになったら、auto-completeをスタートさせる
+  (add-to-list 'ac-modes 'js2-mode)	;; javascript用：js2-modeになったら、auto-completeをスタートさせる
+  (add-to-list 'ac-modes 'jade-mode)	;; jade用：jade-modeになったら、auto-completeをスタートさせる
+  (add-to-list 'ac-modes 'coffee-mode)  ;; coffescript用：coffee-modeになったら、auto-completeをスタートさせる
+  (add-to-list 'ac-modes 'lua-mode))	;; Lua用：lua-modeになったら、auto-completeをスタートさせる
