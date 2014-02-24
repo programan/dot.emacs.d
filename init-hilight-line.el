c7ddb6a130975c54ce30f4f03bf2aac4dc0c9adf
programan <github@programan.com>
2014-02-24 18:43:57 +0900

    カレント行のアンダーラインの色を設定

--- init-hilight-line.el
+++ init-hilight-line.el
@@ -7,15 +7,23 @@
 (defface hlline-face
   '((((class color)
       (background dark))
-     (:background "midnight blue"))
+     ;; (:background "midnight blue"))
+     ;; (:background "gray30"))
+     ;; (:background "dark slate gray"))
+     ;; (:background "gray10"
+     ;;              :underline "gray24"))
+     (:background nil
+                  :underline "RoyalBlue1"))
+
     (((class color)
       (background light))
      (:background "ForestGreen"))
     (t
      ()))
   "*Face used by hl-line.")
-;;(setq hl-line-face 'hlline-face)
-(setq hl-line-face 'underline) ; 下線
+
+(setq hl-line-face 'hlline-face)
+;; (setq hl-line-face 'underline) ; 下線
 (global-hl-line-mode)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
