c1ca9f5bf5a726796d19752b68db2024b3676850
programan <github@programan.com>
2016-02-19 20:15:00 +0900

    recentfの設定を変更

--- init.el
+++ init.el
@@ -216,8 +216,15 @@
 ;;   (exec-path-from-shell-initialize))
 
 ;; recentf
+(when (require 'recentf nil t)
+  (setq recentf-max-saved-items 2000)
+  (setq recentf-exclude '(".recentf"))
+  (setq recentf-auto-cleanup 10)
+  (setq recentf-auto-save-timer
+        (run-with-idle-timer 30 t 'recentf-save-list))
+  (recentf-mode 1))
 ;;(setq recentf-save-file "~/.emacs.d/.recentf")
-(setq recentf-max-saved-items 100)            ;; recentf に保存するファイルの数
-(setq recentf-exclude '(".recentf"))           ;; .recentf自体は含まない
-(setq recentf-auto-cleanup 10)                 ;; 保存する内容を整理
+;;(setq recentf-max-saved-items 100)            ;; recentf に保存するファイルの数
+;;(setq recentf-exclude '(".recentf"))           ;; .recentf自体は含まない
+;;(setq recentf-auto-cleanup 10)                 ;; 保存する内容を整理
 ;;(run-with-idle-timer 30 t 'recentf-save-list)  ;; 30秒ごとに .recentf を保存
