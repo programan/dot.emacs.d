0ce3d42163a3c89a7798c0799e719b9bb60ee84a
programan <github@programan.com>
2016-10-06 15:03:45 +0900

    update lisp package

--- elpa/python-mode-20161004.1054/python-mode-autoloads.el
+++ elpa/python-mode-20161004.1054/python-mode-autoloads.el
@@ -1,10 +1,10 @@
 ;;; python-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("python-mode-pkg.el" "python-mode.el")
-;;;;;;  (22509 53505 697169 0))
+;;;;;;  (22517 59515 0 0))
 
 ;;;***
 
