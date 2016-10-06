536089e2a0261d9b33d4ca181e861119f8c01a49
programan <github@programan.com>
2016-10-07 01:58:10 +0900

    Update magit, projectile

--- elpa/projectile-20161006.321/projectile-autoloads.el
+++ elpa/projectile-20161006.321/projectile-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "projectile" "projectile.el" (22517 64408 0
+;;;### (autoloads nil "projectile" "projectile.el" (22518 33395 0
 ;;;;;;  0))
 ;;; Generated autoloads from projectile.el
 
