0a54de1fdc8fe5460861beb0f0e4727980f5b056
programan <github@programan.com>
2016-10-24 11:34:02 +0900

    Update lisp package

--- elpa/kooten-theme-20161002.1540/kooten-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "kooten-theme" "20161002.1540" "Dark color theme" '((emacs "24.1")) :url "http://github.com/kootenpv/emacs-kooten-theme" :keywords '("themes"))
