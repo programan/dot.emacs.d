b93152fad0ebbdf1b1e95ad1dc19785a82a4cf35
programan <github@programan.com>
2017-01-02 16:58:03 +0900

    update lisp files

---
+++ elpa/all-ext-20161216.415/all-ext-autoloads.el
@@ -0,0 +1,15 @@
+;;; all-ext-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("all-ext.el") (22634 1850 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; all-ext-autoloads.el ends here
