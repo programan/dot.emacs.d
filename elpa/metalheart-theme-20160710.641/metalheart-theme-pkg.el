bea75cb186dc91d54d3caae7b486419301b024cc
programan <github@programan.com>
2018-08-17 14:45:14 +0900

    Update lisp packages.

---
+++ elpa/metalheart-theme-20160710.641/metalheart-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "metalheart-theme" "20160710.641" "Low-contrast theme with a dark blue-green background." '((emacs "24")) :commit "ec98ea2c11dc1213dae8cbe1fe0cee73ca138bb2" :authors '(("Martin Haesler")) :maintainer '("Martin Haesler"))
