2b02909765aa54e30acf26a25b0216561a7d074b
programan <github@programan.com>
2018-08-17 14:44:52 +0900

    Remove old lisp packages.

--- elpa/all-ext-20170114.1805/all-ext-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; all-ext-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("all-ext.el") (22662 52256 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; all-ext-autoloads.el ends here
--- elpa/kooten-theme-20161023.205/kooten-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "kooten-theme" "20161023.205" "Dark color theme" '((emacs "24.1")) :url "http://github.com/kootenpv/emacs-kooten-theme" :keywords '("themes"))
