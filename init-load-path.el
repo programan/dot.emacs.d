76ee81983be28fa04b77362a2126add68c3f9e9e
programan <github@programan.com>
2018-08-22 09:46:46 +0900

    Delete unused lisp packages.

--- init-load-path.el
+++ init-load-path.el
@@ -8,15 +8,4 @@
 ;; (add-to-list 'load-path "~/.emacs.d/site-lisp/rhtml")
 ;; yasnippet(コード補完)
 ;; (add-to-list 'load-path "~/.emacs.d/site-lisp/yasnippet")
-;; emacs-nav 
-(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-nav")
-;; cake-php
-(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-cake")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-cake2")
-;; historyf
-(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-historyf")
-;; mmm-mode
-;;(add-to-list 'load-path "~/.emacs.d/site-lisp/mmm-mode")
-;; helm(fork form anything)
-;; (add-to-list 'load-path "~/.emacs.d/site-lisp/helm")
 
