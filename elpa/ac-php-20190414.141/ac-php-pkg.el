01c0ba2644df1edc2bdfb903760159a277759ca3
programan <github@programan.com>
2019-04-29 09:21:01 +0900

    Remove old lisp packages.

--- elpa/ac-php-20190414.141/ac-php-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ac-php" "20190414.141" "Auto Completion source for PHP." '((ac-php-core "2.0") (auto-complete "1.4.0") (yasnippet "0.8.0")) :commit "3e1737a5c7fe335869dc8b81c557871f8c560d0a" :keywords '("completion" "convenience" "intellisense") :authors '(("jim" . "xcwenn@qq.com")) :maintainer '("jim") :url "https://github.com/xcwen/ac-php")
--- elpa/company-php-20190410.223/company-php-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "company-php" "20190410.223" "A company back-end for PHP." '((cl-lib "0.5") (ac-php-core "2.0") (company "0.9")) :commit "3e1737a5c7fe335869dc8b81c557871f8c560d0a" :keywords '("completion" "convenience" "intellisense") :authors '(("jim" . "xcwenn@qq.com")) :maintainer '("jim") :url "https://github.com/xcwen/ac-php")
