4089d3b193db58ab00b53629f06a5e5d4e1bdcb5
programan <github@programan.com>
2019-11-08 09:46:42 +0900

    Remove unused themes.

--- elpa/metalheart-theme-20160710.641/metalheart-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "metalheart-theme" "20160710.641" "Low-contrast theme with a dark blue-green background." '((emacs "24")) :commit "ec98ea2c11dc1213dae8cbe1fe0cee73ca138bb2" :authors '(("Martin Haesler")) :maintainer '("Martin Haesler"))
