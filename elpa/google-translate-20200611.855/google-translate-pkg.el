c501031b2b8989aab60030857ea27f81cf9611e9
programan <github@programan.com>
2020-06-12 10:43:13 +0900

    Update lisp packages.

---
+++ elpa/google-translate-20200611.855/google-translate-pkg.el
@@ -0,0 +1,4 @@
+(define-package "google-translate" "20200611.855" "Emacs interface to Google Translate." 'nil :commit "f750586b9136f0277e62ff17274dfb460de4b4a6")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/helm-core-20200605.647/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20200605.647" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "51ed0e75306ead255c5938c5f50b5cd73dac306f" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
