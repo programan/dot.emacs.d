0fe0e04b43590605c36714c706db6f93e4342d57
programan <github@programan.com>
2020-06-25 16:03:03 +0900

    Update lisp packages.

---
+++ elpa/slime-20200624.2054/slime-pkg.el
@@ -0,0 +1,9 @@
+(define-package "slime" "20200624.2054" "Superior Lisp Interaction Mode for Emacs"
+  '((cl-lib "0.5")
+    (macrostep "0.9"))
+  :commit "7f1ac7ad5c1c3185d73ff262550cfda5f5ffff5f" :keywords
+  '("languages" "lisp" "slime")
+  :url "https://github.com/slime/slime")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
